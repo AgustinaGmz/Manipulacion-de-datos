3d235cbef0525a6aea8f81684e343b56f6c78cc3
AgustinaGimenez <agustina.gmenz@gmail.com>
2022-04-18 09:40:37 -0300

    manipulación de datos

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,121 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => { 
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, 
+    add: function (req, res) {
+        res.render('moviesAdd')  
+    },
+    create: function (req, res) {
+        /* destructuración del body */
+        const { title, rating, release_date, length, awards } = req.body
+        db.Movie.create({
+            title,  
+            rating,
+            release_date,
+            length,
+            awards
+        }).then(movie => {
+            res.redirect('/movies')
+        })
+        .catch((error) => {
+            res.send(error) 
+        })
+    },
+    edit: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+            res.render('moviesEdit', {
+                movie
+            })
+        })
+        .catch((error) => {
+            res.send(error)  
+        })
+    },
+    update: function (req, res) {
+
+        const {title, rating, release_date, length, awards} = req.body
+
+        db.Movie.update({ 
+            title, 
+            rating,
+            release_date,
+            length,
+            awards
+        }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then((result) => {
+                if(result){
+                    res.redirect('/movies')
+                }
+            })
+            .catch((error) => {
+                res.send(error)  
+            })
+    }, 
+    delete: function (req, res) { 
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+            res.render('moviesDelete', {
+                movie
+            })
+        })
+    },
+    destroy: function (req, res) { 
+        db.Movie.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((result) => {
+            console.log(result)
+            res.redirect('/movies')
+        })
+    }
+
+}
+
+
+module.exports = moviesController;
